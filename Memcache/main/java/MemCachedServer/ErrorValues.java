058d2757b3c40a639c22441c329c4f26ae425d67
chiddu bhat <cnbscience@gmail.com>
2020-05-26 14:21:40 -0400

    updated read me and some comments

--- Memcache/main/java/MemCachedServer/ErrorValues.java
+++ Memcache/main/java/MemCachedServer/ErrorValues.java
@@ -5,17 +5,4 @@ package MemCachedServer;
  * @since   May-21-2020
  */
 
-/**
- * This method is used to check if a player has won the game
- * after placing a disc, this method checks 4 possible directions
- * after every move by either players.
- *  1) Horizontal verification (check all the rows)
- *  2) Vertical verifaction (check all the columns)
- *  3)Left diagonal
- *  4)Right diagonal.
- * @param
- * @return true/false
- * @exception
- * @see
- */
 public enum ErrorValues {ERROR, SERVER_ERROR, CLIENT_ERROR};
